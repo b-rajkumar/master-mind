1dc1cfc529c2317d1a45d9c157eab5c470d74d04
Rajkumar <b-rajkumar@users.noreply.github.com>
2023-08-26 22:10:42 +0530

    test: add a test case to authenticate-test

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,7 +1,10 @@
 const isUserPresent = (req, res, next) => {
   const users = req.app.users;
   const username = req.cookies.name;
-  if (users.includes(username)) next();
+  if (users.includes(username)) {
+    next();
+    return;
+  }
 
   res.redirect(302, "/login");
 };
--- tests/middlewares/authenticate-test.js
+++ tests/middlewares/authenticate-test.js
@@ -1,12 +1,23 @@
 const request = require("supertest");
 const { describe, it } = require("node:test");
+const assert = require("assert");
 const createApp = require("../../src/app");
+const { isUserPresent } = require("../../src/middlewares/authenticate");
 
-describe("authenticate", () => {
+describe("isUserPresent", () => {
   // eslint-disable-next-line max-len
   it("should redirect to the login page, when the user details are not provided", (_, done) => {
     const app = createApp();
 
     request(app).get("/").expect(302).expect("location", "/login").end(done);
+  });
+
+  it("should call the next if the user is present", context => {
+    const req = { cookies: { name: "raj" }, app: { users: ["raj"] } };
+    const res = {};
+    const next = context.mock.fn();
+    isUserPresent(req, res, next);
+
+    assert.deepStrictEqual(next.mock.callCount(), 1);
   });
 });
