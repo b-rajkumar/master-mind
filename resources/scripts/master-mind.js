0a50110c9446b0cd208bdbcdb4b7545eda8d82f3
Rajkumar <b-rajkumar@users.noreply.github.com>
2023-08-28 23:48:36 +0530

    refactor: update the style of the board

--- resources/scripts/master-mind.js
+++ resources/scripts/master-mind.js
@@ -1,12 +1,12 @@
 const colorMapping = {
-  R: "red",
-  G: "green",
-  B: "blue",
+  R: "#e80000",
+  G: "#00b600",
+  B: "#0059d7",
   W: "#dddddd",
-  O: "orange",
-  P: "purple",
-  Y: "yellow",
-  N: "brown",
+  O: "#ed7400",
+  P: "#ba0bff",
+  Y: "#ffdd00",
+  N: "#814216",
 };
 
 const generateCircle = (color, radius) => {
@@ -46,7 +46,7 @@ const updateAttempts = (rowNumber, guess) => {
   Array.from(guess).forEach((color, i) => {
     const colorName = colorMapping[color];
     const box = boxes[i];
-    box.innerHTML = generateCircle(colorName, 18);
+    box.innerHTML = generateCircle(colorName, 20);
   });
 };
 
@@ -59,7 +59,7 @@ const updateResult = (rowNumber, R, W) => {
   Array.from(colorCombination).forEach((color, i) => {
     const colorName = colorMapping[color];
     const box = boxes[i];
-    box.innerHTML = generateCircle(colorName, 12);
+    box.innerHTML = generateCircle(colorName, 15);
   });
 };
 
@@ -116,7 +116,80 @@ const displayPlayername = () => {
   usernameContainer.innerText = cookies.name;
 };
 
+const generateBoxElement = (boxType, radius) => {
+  return `<div class="${boxType}">
+ <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40"
+viewBox="0 0 40 40" fill="none">
+ <g filter="url(#filter0_i_109_219)">
+   <g filter="url(#filter1_i_109_219)">
+     <circle cx="20" cy="20" r="${radius}" fill="#474646" />
+   </g>
+ </g>
+ <defs>
+   <filter id="filter0_i_109_219" x="0" y="0" width="40" height="40"
+    filterUnits="userSpaceOnUse"
+     color-interpolation-filters="sRGB">
+     <feFlood flood-opacity="0" result="BackgroundImageFix" />
+     <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" 
+     result="shape" />
+     <feColorMatrix in="SourceAlpha" type="matrix" 
+     values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+       result="hardAlpha" />
+     <feOffset dy="5" />
+     <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+       <feColorMatrix type="matrix"
+  values="0 0 0 0 0.0666667 0 0 0 0 0.0666667 0 0 0 0 0.0666667 0 0 0 0.4 0" />
+  <feBlend mode="normal" in2="shape" result="effect1_innerShadow_109_219" />
+     </filter>
+     <filter id="filter1_i_109_219" x="0" y="0" width="40"
+      height="44" filterUnits="userSpaceOnUse"
+       color-interpolation-filters="sRGB">
+       <feFlood flood-opacity="0" result="BackgroundImageFix" />
+       <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" 
+       result="shape" />
+       <feColorMatrix in="SourceAlpha" type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+         result="hardAlpha" />
+       <feOffset dy="4" />
+       <feGaussianBlur stdDeviation="2" />
+       <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+       <feColorMatrix type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+       <feBlend mode="normal" in2="shape"
+        result="effect1_innerShadow_109_219" />
+     </filter>
+   </defs>
+ </svg>
+</div>`;
+};
+
+const generateRowElement = (boxType, radius) => {
+  const rowElement = document.createElement("div");
+  rowElement.classList.add("row");
+
+  const boxElement = generateBoxElement(boxType, radius);
+  rowElement.innerHTML = boxElement.repeat(5);
+
+  return rowElement;
+};
+
+const renderRows = (attemptsContainer, boxType, radius) => {
+  for (let i = 0; i < 10; i++) {
+    const rowElement = generateRowElement(boxType, radius);
+    attemptsContainer.append(rowElement);
+  }
+};
+
+const renderBoard = () => {
+  const attemptsContainer = document.querySelector("#attempts");
+  const resultContainer = document.querySelector("#result");
+
+  renderRows(attemptsContainer, "box", 20);
+  renderRows(resultContainer, "small-box", 15);
+};
+
 const main = () => {
+  renderBoard();
   startGame();
   displayPlayername();
   setupSubmitButton();
