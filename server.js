75de7b3ff6299ea660a05b3a5f6c72670f8f7b1e
Rajkumar <b-rajkumar@users.noreply.github.com>
2023-08-29 13:46:46 +0530

    feature: create the database before on server start

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { createApp } = require("./src/app");
-const { readFile, writeFile } = require("fs");
+const fs = require("fs");
+const { readFile, writeFile } = fs;
 const MasterMindDB = require("./src/models/master-mind-db");
 
 const setupDataBase = handler => {
@@ -15,9 +16,21 @@ const setupDataBase = handler => {
   });
 };
 
-const main = () => {
-  const port = 9090;
+const createMasterMindDataBase = () => {
+  const userDetailsFilePath = "./data/users.json";
+  const playerStatsFilePath = "./data/players-stats.json";
 
+  if (!fs.existsSync("./data/")) fs.mkdirSync("./data");
+  if (!fs.existsSync(userDetailsFilePath))
+    fs.writeFileSync(userDetailsFilePath, "[]");
+
+  if (!fs.existsSync(playerStatsFilePath))
+    fs.writeFileSync(playerStatsFilePath, "{}");
+};
+
+const main = () => {
+  createMasterMindDataBase();
+  const port = 9090;
   setupDataBase(masterMindDB => {
     const app = createApp(masterMindDB);
 
